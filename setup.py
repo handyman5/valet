1ec55461faa176c5035292533c4423b32f09db77
Adam Compton <comptona@gmail.com>
2012-08-26 23:22:06 -0700

    Setting things up for distutils and pip

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+from distutils.core import setup
+
+setup(
+	name = "valet",
+	author = "Adam Compton",
+	author_email = "comptona@gmail.com",
+	version = "0.1",
+	license = "BSD-2-Clause",
+	url = "https://github.com/handyman5/valet",
+	download_url = "https://github.com/handyman5/valet/tarball/master",
+	description = "valet is a script that turns any directory into a simple wiki, complete with wikitext rendering and automatically committing changes to version control.",
+        requires = ['bottle'],
+	scripts = ['valet']
+)
