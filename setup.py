d8a593b7a60bf066f658fcb979df888d283c9299
Adam Compton <comptona@gmail.com>
2012-10-28 22:21:23 -0700

    Updated setup.py for pip version bump

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
 	name = "valet",
 	author = "Adam Compton",
 	author_email = "comptona@gmail.com",
-	version = "0.2",
+	version = "0.3",
 	license = "BSD-2-Clause",
 	url = "https://github.com/handyman5/valet",
 	download_url = "https://github.com/handyman5/valet/tarball/master",
-	description = "valet is a script that turns any directory into a simple wiki, complete with wikitext rendering and automatically committing changes to version control.",
+	description = "valet is a script that turns any directory into a simple wiki, complete with wikitext rendering, editing, and automatically committing changes to version control.",
         install_requires = ['bottle'],
 	scripts = ['valet']
 )
